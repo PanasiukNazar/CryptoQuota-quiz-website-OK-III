ae3d5231933a209d6053c7b9a42341f47bec848f
NazarPanasiuk <nazar.panasiuk.ua@gmail.com>
2024-07-02 00:36:46 +0300

    WIP

--- assets/vendor/jquery-circle-progress/circle.js
+++ assets/vendor/jquery-circle-progress/circle.js
@@ -8,14 +8,23 @@ function animateElements() {
         var animate = $(this).data('animate');
         if (elementPos < topOfWindow + $(window).height() - 30 && !animate) {
             $(this).data('animate', true);
-            $(this).find('.circle').circleProgress({
-                startAngle: -Math.PI / 2,
-                value: percent / 100, 
-                lineCap:"round",
-                size: 180 ,
-                }).on('circle-animation-progress', function (event, progress, stepValue) {
-                $(this).find('div').text((stepValue*100).toFixed(1) + "%");
-            }).stop();
+            $(this)
+                .find('.circle')
+                .circleProgress({
+                    startAngle: -Math.PI / 2,
+                    value: percent / 100,
+                    lineCap: 'round',
+                    size: 180,
+                })
+                .on(
+                    'circle-animation-progress',
+                    function (event, progress, stepValue) {
+                        $(this)
+                            .find('div')
+                            .text((stepValue * 100).toFixed(1) + '%');
+                    },
+                )
+                .stop();
         }
     });
 }
@@ -23,36 +32,3 @@ function animateElements() {
 // Show animated elements
 animateElements();
 $(window).scroll(animateElements);
-    
-// counter up   
-   var a = 0;
-   $(window).scroll(function() {
-
-    var oTop = $('.fables-counter').offset().top - window.innerHeight;
-    if (a == 0 && $(window).scrollTop() > oTop) {
-      $('.fables-counter-value').each(function() {
-        var $this = $(this),
-          countTo = $this.attr('data-count');
-        $({
-          countNum: $this.text()
-        }).animate({
-            countNum: countTo
-          },
-
-          {
-            duration: 4000,
-            easing: 'swing',
-            step: function() {
-              $this.text(Math.floor(this.countNum));
-            },
-            complete: function() {
-              $this.text(this.countNum);
-              //alert('finished');
-            }
-
-          });
-      });
-      a = 1;
-    }
-
-   });   
